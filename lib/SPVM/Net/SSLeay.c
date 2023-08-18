58e1bd0f690746af1ced61b8503156acf68d1b14
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-08-19 08:18:29 +0900

    First commit.

---
+++ lib/SPVM/Net/SSLeay.c
@@ -0,0 +1,15 @@
+// Copyright (c) 2023 [--user-name]
+// MIT License
+
+#include "spvm_native.h"
+
+
+
+static const char* FILE_NAME = "Net/SSLeay.c";
+
+int32_t SPVM__Net__SSLeay__foo(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  return 0;
+}
+
+
---
+++ lib/SPVM/Net/SSLeay.pm
@@ -0,0 +1,45 @@
+package SPVM::Net::SSLeay;
+
+our $VERSION = "0.001";
+
+1;
+
+=head1 Name
+
+SPVM::Net::SSLeay - Short Description
+
+=head1 Description
+
+The Net::SSLeay class of L<SPVM> has methods for someting.
+
+=head1 Usage
+
+  use Net::SSLeay;
+
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+[--user-name] C<[--user-email]>
+
+=head1 Copyright & License
+
+Copyright (c) 2023 [--user-name]
+
+MIT License
+
