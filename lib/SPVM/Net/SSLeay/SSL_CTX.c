963b8a6ce8e9219657e61bee087f30eab2e285be
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-08-23 08:51:52 +0900

    Net::SSLeay::SSL_CTX is added.

---
+++ lib/SPVM/Net/SSLeay/SSL_CTX.c
@@ -0,0 +1,15 @@
+// Copyright (c) 2023 Yuki Kimoto
+// MIT License
+
+#include "spvm_native.h"
+
+
+
+static const char* FILE_NAME = "Net/SSLeay/SSL_CTX.c";
+
+int32_t SPVM__Net__SSLeay__SSL_CTX__foo(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  return 0;
+}
+
+
