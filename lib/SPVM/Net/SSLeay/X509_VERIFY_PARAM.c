3132ce308c30a31032b7a72b9532ecefcf648a92
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-08-23 10:58:16 +0900

    Net::SSLeay::X509_VERIFY_PARAM is added.

---
+++ lib/SPVM/Net/SSLeay/X509_VERIFY_PARAM.c
@@ -0,0 +1,15 @@
+// Copyright (c) 2023 Yuki Kimoto
+// MIT License
+
+#include "spvm_native.h"
+
+
+
+static const char* FILE_NAME = "Net/SSLeay/X509_VERIFY_PARAM.c";
+
+int32_t SPVM__Net__SSLeay__X509_VERIFY_PARAM__foo(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  return 0;
+}
+
+
---
+++ lib/SPVM/Net/SSLeay/X509_VERIFY_PARAM.pm
@@ -0,0 +1,45 @@
+package SPVM::Net::SSLeay::X509_VERIFY_PARAM;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::Net::SSLeay::X509_VERIFY_PARAM - Short Description
+
+=head1 Description
+
+The Net::SSLeay::X509_VERIFY_PARAM class of L<SPVM> has methods for someting.
+
+=head1 Usage
+
+  use Net::SSLeay::X509_VERIFY_PARAM;
+
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+Yuki Kimoto C<kimoto.yuki@gmail.com>
+
+=head1 Copyright & License
+
+Copyright (c) 2023 Yuki Kimoto
+
+MIT License
+
