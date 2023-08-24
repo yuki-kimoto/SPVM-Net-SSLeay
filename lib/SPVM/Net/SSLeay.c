55f1b1bbb5184398a9d5fe7040094be1ae074ce0
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-08-24 09:36:25 +0900

    set_tlsext_host_name is added to Net::SSLeay.

--- lib/SPVM/Net/SSLeay.c
+++ lib/SPVM/Net/SSLeay.c
@@ -37,6 +37,19 @@ int32_t SPVM__Net__SSLeay__new(SPVM_ENV* env, SPVM_VALUE* stack) {
   return 0;
 }
 
+int32_t SPVM__Net__SSLeay__DESTROY(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  int32_t error_id = 0;
+  
+  void* obj_self = stack[0].oval;
+  
+  SSL* ssl = env->get_pointer(env, stack, obj_self);
+  
+  SSL_free(ssl);
+  
+  return 0;
+}
+
 int32_t SPVM__Net__SSLeay__connect(SPVM_ENV* env, SPVM_VALUE* stack) {
   
   int32_t error_id = 0;
@@ -48,7 +61,8 @@ int32_t SPVM__Net__SSLeay__connect(SPVM_ENV* env, SPVM_VALUE* stack) {
   int32_t status = SSL_connect(ssl);
   
   if (!(status == 1)) {
-    return env->die(env, stack, "[System Error]SSL_connect failed.", __func__, FILE_NAME, __LINE__);
+    env->die(env, stack, "[System Error]SSL_connect failed.", __func__, FILE_NAME, __LINE__);
+    return SPVM_NATIVE_C_BASIC_TYPE_ID_ERROR_SYSTEM_CLASS;
   }
   
   return 0;
@@ -65,7 +79,8 @@ int32_t SPVM__Net__SSLeay__accept(SPVM_ENV* env, SPVM_VALUE* stack) {
   int32_t status = SSL_accept(ssl);
   
   if (!(status == 1)) {
-    return env->die(env, stack, "[System Error]SSL_accept failed.", __func__, FILE_NAME, __LINE__);
+    env->die(env, stack, "[System Error]SSL_accept failed.", __func__, FILE_NAME, __LINE__);
+    return SPVM_NATIVE_C_BASIC_TYPE_ID_ERROR_SYSTEM_CLASS;
   }
   
   return 0;
@@ -82,7 +97,8 @@ int32_t SPVM__Net__SSLeay__shutdown(SPVM_ENV* env, SPVM_VALUE* stack) {
   int32_t status = SSL_shutdown(ssl);
   
   if (!(status == 1)) {
-    return env->die(env, stack, "[System Error]SSL_shutdown failed.", __func__, FILE_NAME, __LINE__);
+    env->die(env, stack, "[System Error]SSL_shutdown failed.", __func__, FILE_NAME, __LINE__);
+    return SPVM_NATIVE_C_BASIC_TYPE_ID_ERROR_SYSTEM_CLASS;
   }
   
   return 0;
@@ -101,21 +117,36 @@ int32_t SPVM__Net__SSLeay__set_fd(SPVM_ENV* env, SPVM_VALUE* stack) {
   int32_t status = SSL_set_fd(ssl, fd);
   
   if (!(status == 1)) {
-    return env->die(env, stack, "[System Error]SSL_set_fd failed.", __func__, FILE_NAME, __LINE__);
+    env->die(env, stack, "[System Error]SSL_set_fd failed.", __func__, FILE_NAME, __LINE__);
+    return SPVM_NATIVE_C_BASIC_TYPE_ID_ERROR_SYSTEM_CLASS;
   }
   
   return 0;
 }
 
-int32_t SPVM__Net__SSLeay__DESTROY(SPVM_ENV* env, SPVM_VALUE* stack) {
+int32_t SPVM__Net__SSLeay__set_tlsext_host_name(SPVM_ENV* env, SPVM_VALUE* stack) {
   
   int32_t error_id = 0;
   
   void* obj_self = stack[0].oval;
   
+  void* obj_name = stack[1].oval;
+  
+  if (!obj_name) {
+    return env->die(env, stack, "The $name must be defined.", __func__, FILE_NAME, __LINE__);
+  }
+  
+  const char* name = env->get_chars(env, stack, obj_name);
+  
   SSL* ssl = env->get_pointer(env, stack, obj_self);
   
-  SSL_free(ssl);
+  int32_t status = SSL_set_tlsext_host_name(ssl, name);
+  
+  if (!(status == 1)) {
+    env->die(env, stack, "[System Error]SSL_set_tlsext_host_name failed.", __func__, FILE_NAME, __LINE__);
+    return SPVM_NATIVE_C_BASIC_TYPE_ID_ERROR_SYSTEM_CLASS;
+  }
   
   return 0;
 }
+
