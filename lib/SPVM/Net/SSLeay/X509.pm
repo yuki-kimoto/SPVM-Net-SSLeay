2d5b151bf9753dd3f913784dad4fb10ceaa7ee29
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-08-31 09:02:42 +0900

    The doc of X509 is improved.

--- lib/SPVM/Net/SSLeay/X509.pm
+++ lib/SPVM/Net/SSLeay/X509.pm
@@ -6,36 +6,15 @@ package SPVM::Net::SSLeay::X509;
 
 =head1 Name
 
-SPVM::Net::SSLeay::X509 - Short Description
+SPVM::Net::SSLeay::X509 - X509
 
 =head1 Description
 
-The Net::SSLeay::X509 class of L<SPVM> has methods for someting.
+The Net::SSLeay::X509 class of L<SPVM> has methods to manipulate X509
 
 =head1 Usage
 
   use Net::SSLeay::X509;
-
-=head1 Fields
-
-
-
-=head1 Class Methods
-
-
-
-=head1 Instance Methods
-
-
-
-
-=head1 Repository
-
-
-
-=head1 Author
-
-Yuki Kimoto C<kimoto.yuki@gmail.com>
 
 =head1 Copyright & License
 
