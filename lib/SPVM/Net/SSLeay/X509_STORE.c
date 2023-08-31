d376f64b4829361bf4820715df99fbda8dac61a1
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-08-31 09:08:41 +0900

    X509_STORE is added.

---
+++ lib/SPVM/Net/SSLeay/X509_STORE.c
@@ -0,0 +1,15 @@
+// Copyright (c) 2023 Yuki Kimoto
+// MIT License
+
+#include "spvm_native.h"
+
+
+
+static const char* FILE_NAME = "Net/SSLeay/X509_STORE.c";
+
+int32_t SPVM__Net__SSLeay__X509_STORE__foo(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  return 0;
+}
+
+
