8367d02768f915b66775bc3fbb22046557d0695f
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-10-31 07:48:35 +0900

    Add Net::SSLeay::X509_STORE#set_flags method.

--- lib/SPVM/Net/SSLeay/X509_STORE.c
+++ lib/SPVM/Net/SSLeay/X509_STORE.c
@@ -28,4 +28,21 @@ int32_t SPVM__Net__SSLeay__X509_STORE__add_cert(SPVM_ENV* env, SPVM_VALUE* stack
   return 0;
 }
 
+int32_t SPVM__Net__SSLeay__X509_STORE__set_flags(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  int32_t error_id = 0;
+  
+  void* obj_self = stack[0].oval;
+  X509_STORE* x509_store = env->get_pointer(env, stack, obj_self);
+  
+  int64_t flags = stack[1].lval;
+  
+  int32_t status = X509_STORE_set_flags(x509_store, flags);
+  
+  if (!(status == 1)) {
+    return env->die(env, stack, "X509_STORE_set_flags failed.", __func__, FILE_NAME, __LINE__);
+  }
+  
+  return 0;
+}
 
--- lib/SPVM/Net/SSLeay/X509_STORE.pm
+++ lib/SPVM/Net/SSLeay/X509_STORE.pm
@@ -6,36 +6,25 @@ package SPVM::Net::SSLeay::X509_STORE;
 
 =head1 Name
 
-SPVM::Net::SSLeay::X509_STORE - Short Description
+SPVM::Net::SSLeay::X509_STORE - X509_STORE
 
 =head1 Description
 
-The Net::SSLeay::X509_STORE class of L<SPVM> has methods for someting.
+Net::SSLeay::X509_STORE in L<SPVM> represetns C<X509_STORE> structure in OpenSSL.
 
 =head1 Usage
 
   use Net::SSLeay::X509_STORE;
 
-=head1 Fields
-
-
-
-=head1 Class Methods
-
-
-
 =head1 Instance Methods
 
+=head2 add_cert
 
+C<method add_cert : int ($x : L<Net::SSLeay::X509|SPVM::Net::SSLeay::X509>);>
 
+=head2 set_flags
 
-=head1 Repository
-
-
-
-=head1 Author
-
-Yuki Kimoto C<kimoto.yuki@gmail.com>
+C<method set_flags : void ($flags : long);>
 
 =head1 Copyright & License
 
