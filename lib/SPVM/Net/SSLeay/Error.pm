2e545f23827c924e1f8fa0aeea247551a1fb470c
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-11-07 11:57:08 +0900

    Add Net::SSLeay::Error class.

---
+++ lib/SPVM/Net/SSLeay/Error.pm
@@ -0,0 +1,28 @@
+package SPVM::Net::SSLeay::Error;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::Net::SSLeay::Error - Short Description
+
+=head1 Description
+
+Net::SSLeay::Error class in L<SPVM> represents an OpenSSL error.
+
+=head1 Usage
+
+  use Net::SSLeay::Error;
+
+=head1 Author
+
+Yuki Kimoto C<kimoto.yuki@gmail.com>
+
+=head1 Copyright & License
+
+Copyright (c) 2024 Yuki Kimoto
+
+MIT License
+
