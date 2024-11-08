1904e8b5aa0bc75b080912b52f8cbf3697ed08af
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-11-09 07:09:24 +0900

    Add Net::SSLeay::Callback::Pem_password_cb interface.

---
+++ lib/SPVM/Net/SSLeay/Callback/Pem_password_cb.pm
@@ -0,0 +1,41 @@
+package SPVM::Net::SSLeay::Callback::Pem_password_cb;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::Net::SSLeay::Callback::Pem_password_cb - Short Description
+
+=head1 Description
+
+Net::SSLeay::Callback::Pem_password_cb interface in L<SPVM> has interface methods to do someting.
+
+=head1 Usage
+
+  interface Net::SSLeay::Callback::Pem_password_cb;
+
+=head1 Details
+
+
+
+=head1 Interface Methods
+
+C<required method : int ($buf : string, $size : int, $rwflag : int, $u : object);>
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+Yuki Kimoto C<kimoto.yuki@gmail.com>
+
+=head1 Copyright & License
+
+Copyright (c) 2024 Yuki Kimoto
+
+MIT License
+
