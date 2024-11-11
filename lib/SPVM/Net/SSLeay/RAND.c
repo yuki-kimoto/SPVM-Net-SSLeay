44696695321e380bb5d2e22455f4b5d8599aee82
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-11-11 14:30:14 +0900

    Add Net::SSLeay::RAND#seed method.

--- lib/SPVM/Net/SSLeay/RAND.c
+++ lib/SPVM/Net/SSLeay/RAND.c
@@ -3,13 +3,27 @@
 
 #include "spvm_native.h"
 
-
+#include <openssl/ssl.h>
+#include <openssl/err.h>
 
 static const char* FILE_NAME = "Net/SSLeay/RAND.c";
 
-int32_t SPVM__Net__SSLeay__RAND__foo(SPVM_ENV* env, SPVM_VALUE* stack) {
+int32_t SPVM__Net__SSLeay__RAND__seed(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  int32_t error_id = 0;
+  
+  void* obj_buf = stack[0].oval;
+  
+  int32_t num = stack[1].ival;
+  
+  if (!obj_buf) {
+    return env->die(env, stack, "The buffer $buf must be defined.", __func__, FILE_NAME, __LINE__);
+  }
+  
+  const char* buf = env->get_chars(env, stack, obj_buf);
+  
+  RAND_seed(buf, num);
   
   return 0;
 }
-
 
--- lib/SPVM/Net/SSLeay/RAND.pm
+++ lib/SPVM/Net/SSLeay/RAND.pm
@@ -18,7 +18,15 @@ Net::SSLeay::RAND class in L<SPVM> represents RAND name space in OpenSSL.
 
 =head1 Class Methods
 
+=head2 seed
 
+C<static method seed : void ($buf : string, $num : int);>
+
+Calls native L<RAND_seed|https://docs.openssl.org/3.0/man3/RAND_add/> function.
+
+Exceptions:
+
+The buffer $buf must be defined. Otherwise an exception is thrown.
 
 =head1 See Also
 
