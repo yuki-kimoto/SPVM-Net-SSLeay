048d8bb1e183e5b98980aa21374f1d8bf62fa969
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-11-11 12:55:50 +0900

    Add Net::SSLeay::OPENSSL class.

---
+++ lib/SPVM/Net/SSLeay/OPENSSL.c
@@ -0,0 +1,16 @@
+// Copyright (c) 2024 Yuki Kimoto
+// MIT License
+
+#include "spvm_native.h"
+
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
+static const char* FILE_NAME = "Net/SSLeay/OPENSSL.c";
+
+int32_t SPVM__Net__SSLeay__OPENSSL__foo(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  return 0;
+}
+
+
---
+++ lib/SPVM/Net/SSLeay/OPENSSL.pm
@@ -0,0 +1,36 @@
+package SPVM::Net::SSLeay::OPENSSL;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::Net::SSLeay::OPENSSL - OPENSSL Name Space in OpenSSL
+
+=head1 Description
+
+Net::SSLeay::OPENSSL class in L<SPVM> represetns L<https://docs.openssl.org/1.1.1/man3/OPENSSL_init_sslOPENSSL> Name Space in OpenSSL.
+
+=head1 Usage
+
+  use Net::SSLeay::OPENSSL;
+
+=head1 Class Methods
+
+
+
+=head1 See Also
+
+=over 2
+
+=item * L<Net::SSLeay|SPVM::Net::SSLeay>
+
+=back
+
+=head1 Copyright & License
+
+Copyright (c) 2024 Yuki Kimoto
+
+MIT License
+
