2fc052d5b07d7a2b372e92e09d65121c67c764de
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-11-11 14:25:16 +0900

    Add Net::SSLeay::RAND class.

---
+++ lib/SPVM/Net/SSLeay/RAND.pm
@@ -0,0 +1,36 @@
+package SPVM::Net::SSLeay::RAND;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::Net::SSLeay::RAND - RAND Name Space in OpenSSL
+
+=head1 Description
+
+Net::SSLeay::RAND class in L<SPVM> represents RAND name space in OpenSSL.
+
+=head1 Usage
+
+  use Net::SSLeay::RAND;
+
+=head1 Class Methods
+
+
+
+=head1 See Also
+
+=over 2
+
+=item * L<Net::SSLeay|SPVM::Net::SSLeay>
+
+=back
+
+=head1 Copyright & License
+
+Copyright (c) 2024 Yuki Kimoto
+
+MIT License
+
