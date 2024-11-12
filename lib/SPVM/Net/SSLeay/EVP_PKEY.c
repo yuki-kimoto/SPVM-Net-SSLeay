408a77921f2ac7ffa317691806bf25163a7d89cf
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-11-12 10:16:35 +0900

    Add Net::SSLeay::EVP_PKEY#DESTROY method.

--- lib/SPVM/Net/SSLeay/EVP_PKEY.c
+++ lib/SPVM/Net/SSLeay/EVP_PKEY.c
@@ -8,9 +8,18 @@
 
 static const char* FILE_NAME = "Net/SSLeay/EVP_PKEY.c";
 
-int32_t SPVM__Net__SSLeay__EVP_PKEY__foo(SPVM_ENV* env, SPVM_VALUE* stack) {
+int32_t SPVM__Net__SSLeay__X509__DESTROY(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  int32_t error_id = 0;
+  
+  void* obj_self = stack[0].oval;
+  
+  X509* x509 = env->get_pointer(env, stack, obj_self);
+  
+  if (!env->no_free(env, stack, obj_self)) {
+    X509_free(x509);
+  }
   
   return 0;
 }
-
 
--- lib/SPVM/Net/SSLeay/EVP_PKEY.pm
+++ lib/SPVM/Net/SSLeay/EVP_PKEY.pm
@@ -10,19 +10,19 @@ SPVM::Net::SSLeay::EVP_PKEY - EVP_PKEY Data Structure in OpenSSL
 
 =head1 Description
 
-Net::SSLeay::EVP_PKEY class in L<SPVM> represents EVP_PKEY data structure in OpenSSL.
+Net::SSLeay::EVP_PKEY class in L<SPVM> represents L<EVP_PKEY|https://docs.openssl.org/3.0/man3/EVP_PKEY_new/> data structure in OpenSSL.
 
 =head1 Usage
 
   use Net::SSLeay::EVP_PKEY;
 
-=head1 Class Methods
-
-
-
 =head1 Instance Methods
 
+=head2 DESTROY
 
+C<method DESTROY : void ();>
+
+Frees native L<EVP_PKEY|https://docs.openssl.org/3.0/man3/EVP_PKEY_new/> object by calling native L<EVP_PKEY_free|https://docs.openssl.org/3.0/man3/EVP_PKEY_new/> function if C<no_free> flag of the instance is not a true value.
 
 =head1 See Also
 
