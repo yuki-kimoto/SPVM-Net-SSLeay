53abd93b4e1bb4ce0bb7251a787eb033fb752268
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-11-12 13:09:00 +0900

    Add Net::SSLeay::OBJ class.

---
+++ lib/SPVM/Net/SSLeay/OBJ.c
@@ -0,0 +1,15 @@
+// Copyright (c) 2024 Yuki Kimoto
+// MIT License
+
+#include "spvm_native.h"
+
+
+
+static const char* FILE_NAME = "Net/SSLeay/OBJ.c";
+
+int32_t SPVM__Net__SSLeay__OBJ__foo(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  return 0;
+}
+
+
---
+++ lib/SPVM/Net/SSLeay/OBJ.pm
@@ -0,0 +1,36 @@
+package SPVM::Net::SSLeay::OBJ;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::Net::SSLeay::OBJ - OBJ Name Space in OpenSSL
+
+=head1 Description
+
+Net::SSLeay::OBJ class in L<SPVM> represents OBJ name space in OpenSSL
+
+=head1 Usage
+
+  use Net::SSLeay::OBJ;
+
+=head1 Class Methods
+
+=head1 See Also
+
+=over 2
+
+=item * L<Net::SSLeay::PEM|SPVM::Net::SSLeay::ASN1_OBJECT>
+
+=item * L<Net::SSLeay|SPVM::Net::SSLeay>
+
+=back
+
+=head1 Copyright & License
+
+Copyright (c) 2024 Yuki Kimoto
+
+MIT License
+
