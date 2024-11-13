814e44b0a8c8a797d11655b7ea39485ff879114b
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-11-13 12:53:02 +0900

    Use a constructor.

--- lib/SPVM/Net/SSLeay.c
+++ lib/SPVM/Net/SSLeay.c
@@ -724,8 +724,12 @@ int32_t SPVM__Net__SSLeay__get_peer_certificate(SPVM_ENV* env, SPVM_VALUE* stack
   void* obj_x509 = NULL;
   
   if (x509) {
-    env->new_pointer_object_by_name(env, stack, "Net::SSLeay::X509", x509, &error_id, __func__, FILE_NAME, __LINE__);
+    void* obj_address_x509 = env->new_pointer_object_by_name(env, stack, "Address", x509, &error_id, __func__, FILE_NAME, __LINE__);
     if (error_id) { return error_id; }
+    stack[0].oval = obj_address_x509;
+    env->call_class_method_by_name(env, stack, "Net::SSLeay::X509", "new_with_pointer", 1, &error_id, __func__, FILE_NAME, __LINE__);
+    if (error_id) { return error_id; }
+    obj_x509 = stack[0].oval;
   }
   
   stack[0].oval = obj_x509;
