a0fba7f4d460e50f8118b420e7250d174cfd5983
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-11-13 10:46:16 +0900

    Add Net::SSLeay::DER#d2i_OCSP_RESPONSE method.

--- lib/SPVM/Net/SSLeay/DER.pm
+++ lib/SPVM/Net/SSLeay/DER.pm
@@ -36,6 +36,24 @@ $ppin_ref at index 0 must be defined. Otherwise an exception is thrown. Otherwis
 
 If d2i_OCSP_REQUEST failed, an exception is thrown with C<eval_error_id> set to the basic type ID of L<Net::SSLeay::Error|SPVM::Net::SSLeay::Error> class.
 
+=head2 d2i_OCSP_RESPONSE
+
+C<static method d2i_OCSP_RESPONSE : L<Net::SSLeay::OCSP_RESPONSE|SPVM::Net::SSLeay::OCSP_RESPONSE> ($a_ref : L<Net::SSLeay::OCSP_RESPONSE|SPVM::Net::SSLeay::OCSP_RESPONSE>[], $ppin_ref : string[], $length : long);>
+
+Calls native L<d2i_OCSP_RESPONSE|https://docs.openssl.org/master/man3/d2i_X509> function given $a_ref, $ppin_ref, $length, and creates a new L<Net::SSLeay::OCSP_RESPONSE|SPVM::Net::SSLeay::OCSP_RESPONSE> object, sets the pointer value of the new object to the return value of the native function, and returns the new object.
+
+Exceptions:
+
+$a must be undef(Currently reuse feature is not available). Otherwise an exception is thrown.
+
+$ppin_ref must be defined. Otherwise an exception is thrown. Otherwise an exception is thrown.
+
+The length of $ppin_ref must be 1. Otherwise an exception is thrown. Otherwise an exception is thrown.
+
+$ppin_ref at index 0 must be defined. Otherwise an exception is thrown. Otherwise an exception is thrown.
+
+If d2i_OCSP_RESPONSE failed, an exception is thrown with C<eval_error_id> set to the basic type ID of L<Net::SSLeay::Error|SPVM::Net::SSLeay::Error> class.
+
 =head1 See Also
 
 =over 2
