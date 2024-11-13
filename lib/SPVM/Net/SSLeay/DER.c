29fdbccf12bdba68dae9307e59a94ff214657a32
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-11-13 09:55:38 +0900

    Add Net::SSLeay::DER class.

---
+++ lib/SPVM/Net/SSLeay/DER.c
@@ -0,0 +1,16 @@
+// Copyright (c) 2024 Yuki Kimoto
+// MIT License
+
+#include "spvm_native.h"
+
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
+static const char* FILE_NAME = "Net/SSLeay/DER.c";
+
+int32_t SPVM__Net__SSLeay__DER__foo(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  return 0;
+}
+
+
---
+++ lib/SPVM/Net/SSLeay/DER.pm
@@ -0,0 +1,36 @@
+package SPVM::Net::SSLeay::DER;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::Net::SSLeay::DER - Name Space for d2i_ and i2d_ prefixed functions in OpenSSL.
+
+=head1 Description
+
+Net::SSLeay::DER class in L<SPVM> represents a name space for L<d2i_ and i2d_ prefixed functions|https://docs.openssl.org/master/man3/d2i_X509> in OpenSSL.
+
+=head1 Usage
+
+  use Net::SSLeay::DER;
+
+=head1 Class Methods
+
+
+
+=head1 See Also
+
+=over 2
+
+=item * L<Net::SSLeay|SPVM::Net::SSLeay>
+
+=back
+
+=head1 Copyright & License
+
+Copyright (c) 2024 Yuki Kimoto
+
+MIT License
+
