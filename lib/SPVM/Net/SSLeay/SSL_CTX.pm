271d507347714d6158aa77101ccb96a918325b10
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-11-13 13:50:40 +0900

    Improve docs.

--- lib/SPVM/Net/SSLeay/SSL_CTX.pm
+++ lib/SPVM/Net/SSLeay/SSL_CTX.pm
@@ -44,25 +44,25 @@ If SSL_CTX_new failed, an exception is thrown with C<eval_error_id> set to the b
 
 C<method set_mode : long ($mode : long);>
 
-Adds the mode $mode by calling native L<SSL_CTX_set_mode|https://docs.openssl.org/1.0.2/man3/SSL_CTX_set_mode> function, and returns the updated mode.
+Calls native L<SSL_CTX_set_mode|https://docs.openssl.org/1.0.2/man3/SSL_CTX_set_mode> function given $mode, and returns the updated mode.
 
 =head2 set_verify
 
 C<method set_verify : void ($mode : int);>
 
-Sets the verification flags $mode by calling native L<SSL_CTX_set_verify|https://docs.openssl.org/master/man3/SSL_CTX_set_verify/> function.
+Calls native L<SSL_CTX_set_verify|https://docs.openssl.org/master/man3/SSL_CTX_set_verify/> function given $mode.
 
 =head2 get0_param
 
 C<method get0_param : L<Net::SSLeay::X509_VERIFY_PARAM|SPVM::Net::SSLeay::X509_VERIFY_PARAM> ();>
 
-Creates a L<Net::SSLeay::X509_VERIFY_PARAM|SPVM::Net::SSLeay::X509_VERIFY_PARAM> object, calls L<SSL_CTX_get0_param|https://docs.openssl.org/master/man3/SSL_CTX_get0_param/> function, sets the pointer value of the new object to the return value of the function, and returns the new object.
+Calls native L<SSL_CTX_get0_param|https://docs.openssl.org/master/man3/SSL_CTX_get0_param/> function, creates a L<Net::SSLeay::X509_VERIFY_PARAM|SPVM::Net::SSLeay::X509_VERIFY_PARAM> object, sets the pointer value of the new object to the return value of the native function, and returns the new object.
 
 =head2 load_verify_locations
 
 C<method load_verify_locations : int ($path : string);>
 
-Specifies the locations, at which CA certificates for verification purposes are located by calling native L<SSL_CTX_load_verify_locations|https://docs.openssl.org/master/man3/SSL_CTX_load_verify_locations/> function.
+Calls native L<SSL_CTX_load_verify_locations|https://docs.openssl.org/master/man3/SSL_CTX_load_verify_locations/> function given $path, and returns its return value.
 
 Exceptions:
 
@@ -72,7 +72,7 @@ If SSL_CTX_load_verify_locations failed, an exception is thrown with C<eval_erro
 
 C<method set_default_verify_paths : int ();>
 
-Specifies that the default locations from which CA certificates are loaded should be used by calling native L<set_default_verify_paths|https://docs.openssl.org/master/man3/SSL_CTX_load_verify_locations/> function.
+Calls native L<set_default_verify_paths|https://docs.openssl.org/master/man3/SSL_CTX_load_verify_locations/> function, and returns its return value.
 
 Exceptions:
 
@@ -82,7 +82,7 @@ If SSL_CTX_set_default_verify_paths failed, an exception is thrown with C<eval_e
 
 C<method use_certificate_file : int ($file : string, $type : int);>
 
-Loads the first certificate stored in the file $file and the type $type by calling native L<use_certificate_file|https://docs.openssl.org/master/man3/SSL_CTX_use_certificate/> function.
+Calls native L<use_certificate_file|https://docs.openssl.org/master/man3/SSL_CTX_use_certificate/> function given $file, $type, and returns its return value.
 
 Exceptions:
 
@@ -94,7 +94,7 @@ If SSL_CTX_use_certificate_file failed, an exception is thrown with C<eval_error
 
 C<method use_certificate_chain_file : int ($file : string);>
 
-Loads a certificate chain from the file $file by calling native L<use_certificate_chain_file|https://docs.openssl.org/1.1.1/man3/SSL_CTX_use_certificate/> function.
+Calls native L<use_certificate_chain_file|https://docs.openssl.org/1.1.1/man3/SSL_CTX_use_certificate/> function given $file, and returns its return value.
  
 Exceptions:
 
@@ -116,7 +116,7 @@ If SSL_CTX_use_PrivateKey_file failed, an exception is thrown with C<eval_error_
 
 C<method set_cipher_list : int ($str : string);>
 
-Sets the list of available ciphers by calling native L<set_cipher_list|https://docs.openssl.org/master/man3/SSL_CTX_set_cipher_list/> function.
+Calls native L<set_cipher_list|https://docs.openssl.org/master/man3/SSL_CTX_set_cipher_list/> function given $str, and returns its return value.
 
 Exceptions:
 
@@ -128,7 +128,7 @@ If SSL_CTX_set_cipher_list failed, an exception is thrown with C<eval_error_id> 
 
 C<method set_ciphersuites : int ($str : string);>
 
-Configures the available TLSv1.3 ciphersuites by calling native L<set_ciphersuites|https://docs.openssl.org/master/man3/SSL_CTX_set_cipher_list/> function.
+Calls native L<set_ciphersuites|https://docs.openssl.org/master/man3/SSL_CTX_set_cipher_list/> function given $str, and returns its return value.
 
 Exceptions:
 
@@ -140,25 +140,25 @@ If SSL_CTX_set_ciphersuites failed, an exception is thrown with C<eval_error_id>
 
 C<method get_cert_store : L<Net::SSLeay::X509_STORE|SPVM::Net::SSLeay::X509_STORE> ();>
 
-Creates a new L<Net::SSLeay::X509_STORE|SPVM::Net::SSLeay::X509_STORE>, calls L<SSL_CTX_set_cert_store|https://docs.openssl.org/master/man3/SSL_CTX_set_cert_store/> function, sets the pointer value of the new object to the return value of the function, and returns the new object.
+Calls native L<SSL_CTX_set_cert_store|https://docs.openssl.org/master/man3/SSL_CTX_set_cert_store/> function, creates a new L<Net::SSLeay::X509_STORE|SPVM::Net::SSLeay::X509_STORE>, sets the pointer value of the new object to the return value of the native function, and returns the new object.
 
 =head2 set_options
 
 C<method set_options : long ($options : long);>
 
-Adds the options set via bitmask in the options $options by calling native L<set_options|https://docs.openssl.org/1.0.2/man3/SSL_CTX_set_options> function, and returns its return value.
+Calls native L<set_options|https://docs.openssl.org/1.0.2/man3/SSL_CTX_set_options> function given $options, and returns its return value.
 
 =head2 get_options
 
 C<method get_options : long ();>
 
-Returns the options by calling native L<SSL_CTX_get_options|https://docs.openssl.org/3.1/man3/SSL_CTX_set_options/> function.
+Calls native L<SSL_CTX_get_options|https://docs.openssl.org/3.1/man3/SSL_CTX_set_options/> function, and returns its return value.
 
 =head2 clear_options
 
 C<method clear_options : long ($options : long);>
 
-Clears the options set via bit-mask in the options $options by calling native L<SSL_CTX_clear_options|https://docs.openssl.org/3.1/man3/SSL_CTX_set_options/> function, and returns its return value.
+Calls native L<SSL_CTX_clear_options|https://docs.openssl.org/3.1/man3/SSL_CTX_set_options/> function given $options, and returns its return value.
 
 =head2 set_alpn_protos
 
