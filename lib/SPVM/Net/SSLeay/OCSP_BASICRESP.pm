0107b5a11162303dee12240870e0bc47ff3e69e6
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-11-14 09:18:51 +0900

    Add Net::SSLeay::OCSP_BASICRESP class.

---
+++ lib/SPVM/Net/SSLeay/OCSP_BASICRESP.pm
@@ -0,0 +1,32 @@
+package SPVM::Net::SSLeay::OCSP_BASICRESP;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::Net::SSLeay::OCSP_BASICRESP - OCSP_BASICRESP Data Strucutre in OpenSSL
+
+=head1 Description
+
+Net::SSLeay::OCSP_BASICRESP class in L<SPVM> represents L<OCSP_BASICRESP|https://docs.openssl.org/1.1.1/man3/OCSP_response_status/> data strucutre in OpenSSL.
+
+=head1 Usage
+
+  use Net::SSLeay::OCSP_BASICRESP;
+
+=head1 See Also
+
+=over 2
+
+=item * L<Net::SSLeay|SPVM::Net::SSLeay>
+
+=back
+
+=head1 Copyright & License
+
+Copyright (c) 2024 Yuki Kimoto
+
+MIT License
+
