7913108a6d194974bf83cc3919e02d9c60105215
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-11-14 09:09:56 +0900

    Add Net::SSLeay::ASN1_GENERALIZEDTIME class.

---
+++ lib/SPVM/Net/SSLeay/ASN1_GENERALIZEDTIME.pm
@@ -0,0 +1,32 @@
+package SPVM::Net::SSLeay::ASN1_GENERALIZEDTIME;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::Net::SSLeay::ASN1_GENERALIZEDTIME - ASN1_GENERALIZEDTIME Data Structure in OpenSSL
+
+=head1 Description
+
+Net::SSLeay::ASN1_GENERALIZEDTIME class in L<SPVM> represents L<ASN1_GENERALIZEDTIME|https://docs.openssl.org/3.2/man3/ASN1_TIME_set> data structure in OpenSSL
+
+=head1 Usage
+
+  use Net::SSLeay::ASN1_GENERALIZEDTIME;
+
+=head1 See Also
+
+=over 2
+
+=item * L<Net::SSLeay|SPVM::Net::SSLeay>
+
+=back
+
+=head1 Copyright & License
+
+Copyright (c) 2024 Yuki Kimoto
+
+MIT License
+
