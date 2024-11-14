3ee2194e2cc4ade4521f9ac981c3113a7943101d
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-11-14 10:26:53 +0900

    Create DER generator.

--- lib/SPVM/Net/SSLeay/DER.pm
+++ lib/SPVM/Net/SSLeay/DER.pm
@@ -16,6 +16,12 @@ Net::SSLeay::DER class in L<SPVM> represents a name space for L<d2i_ and i2d_ pr
 
   use Net::SSLeay::DER;
 
+=head1 Template Methods
+
+=head2 d2i_TYPE
+
+=head2 i2d_TYPE
+
 =head1 Class Methods
 
 =head2 d2i_OCSP_REQUEST
@@ -53,6 +59,24 @@ The length of $ppin_ref must be 1. Otherwise an exception is thrown. Otherwise a
 $ppin_ref at index 0 must be defined. Otherwise an exception is thrown. Otherwise an exception is thrown.
 
 If d2i_OCSP_RESPONSE failed, an exception is thrown with C<eval_error_id> set to the basic type ID of L<Net::SSLeay::Error|SPVM::Net::SSLeay::Error> class.
+
+=head2 d2i_CERTID
+
+C<static method d2i_CERTID : L<Net::SSLeay::CERTID|SPVM::Net::SSLeay::CERTID> ($a_ref : L<Net::SSLeay::CERTID|SPVM::Net::SSLeay::CERTID>[], $ppin_ref : string[], $length : long);>
+
+Calls native L<d2i_CERTID|https://docs.openssl.org/master/man3/d2i_X509> function given $a_ref, $ppin_ref, $length, and creates a new L<Net::SSLeay::CERTID|SPVM::Net::SSLeay::CERTID> object, sets the pointer value of the new object to the return value of the native function, and returns the new object.
+
+Exceptions:
+
+$a_ref must be undef(Currently reuse feature is not available). Otherwise an exception is thrown.
+
+$ppin_ref must be defined. Otherwise an exception is thrown. Otherwise an exception is thrown.
+
+The length of $ppin_ref must be 1. Otherwise an exception is thrown. Otherwise an exception is thrown.
+
+$ppin_ref at index 0 must be defined. Otherwise an exception is thrown. Otherwise an exception is thrown.
+
+If d2i_CERTID failed, an exception is thrown with C<eval_error_id> set to the basic type ID of L<Net::SSLeay::Error|SPVM::Net::SSLeay::Error> class.
 
 =head2 i2d_OCSP_REQUEST
 
---
+++ tmp/DER.pm
@@ -0,0 +1,24 @@
+=head2 d2i_OCSP_REQUEST
+
+C<static method d2i_OCSP_REQUEST : Net::SSLeay::OCSP_REQUEST ($a_ref : Net::SSLeay::OCSP_REQUEST[], $ppin_ref : string[], $length : long);>
+
+See L</"d2i_TYPE"> template method.
+
+=head2 i2d_OCSP_REQUEST
+
+C<static method i2d_OCSP_REQUEST : int ($a : Net::SSLeay::OCSP_REQUEST, $ppout_ref : string[]);>
+
+See L</"i2d_TYPE"> template method.
+
+=head2 d2i_OCSP_RESPONSE
+
+C<static method d2i_OCSP_RESPONSE : Net::SSLeay::OCSP_RESPONSE ($a_ref : Net::SSLeay::OCSP_RESPONSE[], $ppin_ref : string[], $length : long);>
+
+See L</"d2i_TYPE"> template method.
+
+=head2 i2d_OCSP_RESPONSE
+
+C<static method i2d_OCSP_RESPONSE : int ($a : Net::SSLeay::OCSP_RESPONSE, $ppout_ref : string[]);>
+
+See L</"i2d_TYPE"> template method.
+
