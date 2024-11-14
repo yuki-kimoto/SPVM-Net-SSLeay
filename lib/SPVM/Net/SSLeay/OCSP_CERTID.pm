4fb57559a66ddd18e8dc749c07f7cfb113bfadc2
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-11-14 09:22:08 +0900

    Add Net::SSLeay::OCSP_CERTID class.

---
+++ lib/SPVM/Net/SSLeay/OCSP_CERTID.pm
@@ -0,0 +1,32 @@
+package SPVM::Net::SSLeay::OCSP_CERTID;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::Net::SSLeay::OCSP_CERTID - OCSP_CERTID Data Strucutre in OpenSSL
+
+=head1 Description
+
+Net::SSLeay::OCSP_CERTID class in L<SPVM> represents L<OCSP_CERTID|https://docs.openssl.org/1.1.1/man3/OCSP_cert_to_id/> data strucutre in OpenSSL.
+
+=head1 Usage
+
+  use Net::SSLeay::OCSP_CERTID;
+
+=head1 See Also
+
+=over 2
+
+=item * L<Net::SSLeay|SPVM::Net::SSLeay>
+
+=back
+
+=head1 Copyright & License
+
+Copyright (c) 2024 Yuki Kimoto
+
+MIT License
+
