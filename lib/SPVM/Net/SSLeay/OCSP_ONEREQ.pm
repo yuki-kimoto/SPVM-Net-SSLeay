b32894560b2377b1f10983309e67c0fb82c06777
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-11-18 11:10:18 +0900

    Add Net::SSLeay::OCSP_ONEREQ#DESTROY method.

--- lib/SPVM/Net/SSLeay/OCSP_ONEREQ.pm
+++ lib/SPVM/Net/SSLeay/OCSP_ONEREQ.pm
@@ -16,6 +16,14 @@ Net::SSLeay::OCSP_ONEREQ class in L<SPVM> represents L<OCSP_ONEREQ|https://docs.
 
   use Net::SSLeay::OCSP_ONEREQ;
 
+=head1 Instance Methods
+
+=head2 DESTROY
+
+C<method DESTROY : void ();>
+
+Frees native L<OCSP_ONEREQ|https://docs.openssl.org/3.1/man3/X509_dup> object by calling native L<OCSP_ONEREQ_free|https://docs.openssl.org/3.1/man3/X509_dup> function given the pointer of the instance if C<no_free> flag of the instance is not a true value.
+
 =head1 See Also
 
 =over 2
