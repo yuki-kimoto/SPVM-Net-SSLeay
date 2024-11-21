7c1c4549b3c5903d199bfead70ae4b2758ead34c
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-11-21 15:26:51 +0900

    Add  Net::SSLeay::Util::X509#get_ocsp_uri method.

--- lib/SPVM/Net/SSLeay/Util/X509.pm
+++ lib/SPVM/Net/SSLeay/Util/X509.pm
@@ -6,56 +6,37 @@ package SPVM::Net::SSLeay::Util::X509;
 
 =head1 Name
 
-SPVM::Net::SSLeay::Util::X509 - Short Description
+SPVM::Net::SSLeay::Util::X509 - Utilities for X509 Data Structure in OpenSSL
 
 =head1 Description
 
-Net::SSLeay::Util::X509 class in L<SPVM> has methods to do someting.
+Net::SSLeay::Util::X509 class in L<SPVM> has utility methods for X509 data structure in OpenSSL.
 
 =head1 Usage
 
   use Net::SSLeay::Util::X509;
 
-=head1 Details
-
-
-
-=head1 Super Class
-
-
-
-=head1 Interfaces
-
-
-
-=head1 Enumerations
-
-
-
-=head1 Fields
-
-
-
 =head1 Class Methods
 
+C<static method get_ocsp_uri : string ($cert : L<Net::SSLeay::X509|SPVM::Net::SSLeay::X509>);>
 
+Returns OCSP URI in the certificate $cert.
 
-=head1 Instance Methods
+If not found, returns undef.
 
+Exceptions:
 
+The X509 object $cert must be defined. Otherwise an exception is thrown.
 
 =head1 See Also
 
+=over 2
 
+=item * L<Net::SSLeay::X509|SPVM::Net::SSLeay::X509>
 
+=item * L<Net::SSLeay|SPVM::Net::SSLeay>
 
-=head1 Repository
-
-
-
-=head1 Author
-
-Yuki Kimoto C<kimoto.yuki@gmail.com>
+=back
 
 =head1 Copyright & License
 
