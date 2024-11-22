6bd353dead982ff1ebfc7c86ea1c56fee1ebfd03
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-11-22 10:22:52 +0900

    Add the following methods to Net::SSLeay::OPENSSL_INIT class.
      [New Features]
        * Add the following methods to Net::SSLeay::OPENSSL_INIT class.
          static method new : Net::SSLeay::OPENSSL_INIT_SETTINGS ();
    
          static method set_config_filename : int ($init : Net::SSLeay::OPENSSL_INIT_SETTINGS, $filename : string);
    
          static method set_config_file_flags : int ($init : Net::SSLeay::OPENSSL_INIT_SETTINGS, $flags : long);
    
          static method set_config_appname : int ($init : Net::SSLeay::OPENSSL_INIT_SETTINGS, $name : string);

--- lib/SPVM/Net/SSLeay/OPENSSL_INIT.pm
+++ lib/SPVM/Net/SSLeay/OPENSSL_INIT.pm
@@ -18,7 +18,45 @@ Net::SSLeay::OPENSSL_INIT class in L<SPVM> represents L<OPENSSL_INIT|https://doc
 
 =head1 Class Methods
 
+=head2 new
 
+C<static method new : L<Net::SSLeay::OPENSSL_INIT_SETTINGS|SPVM::Net::SSLeay::OPENSSL_INIT_SETTINGS> ();>
+
+Calls native L<OPENSSL_INIT_new|> function, creates a L<Net::SSLeay::OPENSSL_INIT_SETTINGS|SPVM::Net::SSLeay::OPENSSL_INIT_SETTINGS> object, sets the pointer value of the new object to the return value of the native function, and returns the new object.
+
+=head2 set_config_filename
+
+C<static method set_config_filename : int ($init : L<Net::SSLeay::OPENSSL_INIT_SETTINGS|SPVM::Net::SSLeay::OPENSSL_INIT_SETTINGS>, $filename : string);>
+
+Calls native L<OPENSSL_INIT_set_config_filename|https://docs.openssl.org/1.1.1/man3/OPENSSL_init_crypto> function given $filename, and returns its return value.
+
+Requirement:
+
+OpenSSL 3.0
+
+Exceptions:
+
+The file name $filename must be defined. Otherwise an exception is thrown.
+
+=head2 set_config_file_flags
+
+C<static method set_config_file_flags : int ($init : L<Net::SSLeay::OPENSSL_INIT_SETTINGS|SPVM::Net::SSLeay::OPENSSL_INIT_SETTINGS>, $flags : long);>
+
+Calls native L<OPENSSL_INIT_set_config_file_flags|https://docs.openssl.org/1.1.1/man3/OPENSSL_init_crypto> function given $filename, and returns its return value.
+
+Requirement:
+
+OpenSSL 3.0
+
+=head2 set_config_appname
+
+C<static method set_config_appname : int ($init : L<Net::SSLeay::OPENSSL_INIT_SETTINGS|SPVM::Net::SSLeay::OPENSSL_INIT_SETTINGS>, $name : string);>
+
+Calls native L<OPENSSL_INIT_set_config_appname|https://docs.openssl.org/1.1.1/man3/OPENSSL_init_crypto> function given $filename, and returns its return value.
+
+Exceptions:
+
+The app name $name must be defined. Otherwise an exception is thrown.
 
 =head1 See Also
 
