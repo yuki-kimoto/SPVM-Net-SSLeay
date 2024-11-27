54deb1c1bdc006ede540c3360091a57a94701e69
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-11-27 14:07:56 +0900

    Add Net::SSLeay::ASN1_OCTET_STRING class.

---
+++ lib/SPVM/Net/SSLeay/ASN1_OCTET_STRING.pm
@@ -0,0 +1,40 @@
+package SPVM::Net::SSLeay::ASN1_OCTET_STRING;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::Net::SSLeay::ASN1_OCTET_STRING - ASN1_OCTET_STRING Data Structure in OpenSSL
+
+=head1 Description
+
+Net::SSLeay::ASN1_OCTET_STRING class in L<SPVM> represents L<ASN1_OCTET_STRING|https://docs.openssl.org/3.2/man3/X509_new/> data structure in OpenSSL
+
+=head1 Usage
+
+  use Net::SSLeay::ASN1_OCTET_STRING;
+
+=head1 Instance Methods
+
+=head2 DESTROY
+
+C<method DESTROY : void ();>
+
+Calls native L<X509_free|https://docs.openssl.org/3.2/man3/X509_new> function given the pointer value of the instance if C<no_free> flag of the instance is not a true value.
+
+=head1 See Also
+
+=over 2
+
+=item * L<Net::SSLeay|SPVM::Net::SSLeay>
+
+=back
+
+=head1 Copyright & License
+
+Copyright (c) 2024 Yuki Kimoto
+
+MIT License
+
