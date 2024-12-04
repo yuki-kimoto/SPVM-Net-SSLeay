e7541249e22d452336c6d831b264f4d96db40815
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-12-04 12:52:17 +0900

    Add Net::SSLeay::OPENSSL#init_crypto method.

--- lib/SPVM/Net/SSLeay/OPENSSL.c
+++ lib/SPVM/Net/SSLeay/OPENSSL.c
@@ -38,6 +38,36 @@ int32_t SPVM__Net__SSLeay__OPENSSL__add_all_algorithms(SPVM_ENV* env, SPVM_VALUE
   return 0;
 }
 
+int32_t SPVM__Net__SSLeay__OPENSSL__init_crypto(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  int32_t error_id = 0;
+  
+  int64_t opts = stack[0].lval;
+  
+  void* obj_settings = stack[1].oval;
+  
+  OPENSSL_INIT_SETTINGS* settings = NULL;
+  if (obj_settings) {
+    settings = env->get_pointer(env, stack, obj_settings);
+  }
+  
+  int32_t status = OPENSSL_init_crypto(opts, settings);
+  
+  if (!(status == 1)) {
+    env->die(env, stack, "[OpenSSL Error]OPENSSL_init_crypto failed.", __func__, FILE_NAME, __LINE__);
+    
+    int32_t tmp_error_id = env->get_basic_type_id_by_name(env, stack, "Net::SSLeay::Error", &error_id, __func__, FILE_NAME, __LINE__);
+    if (error_id) { return error_id; }
+    error_id = tmp_error_id;
+    
+    return error_id;
+  }
+  
+  stack[0].ival = status;
+  
+  return 0;
+}
+
 int32_t SPVM__Net__SSLeay__OPENSSL__init_ssl(SPVM_ENV* env, SPVM_VALUE* stack) {
   
   int32_t error_id = 0;
