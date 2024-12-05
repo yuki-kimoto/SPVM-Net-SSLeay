6c1a3dbafeca200a0df9b53857ff304aa848c212
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-12-06 08:14:59 +0900

    Add Net::SSLeay::X509_CRL#new method.

--- lib/SPVM/Net/SSLeay/X509_CRL.pm
+++ lib/SPVM/Net/SSLeay/X509_CRL.pm
@@ -16,6 +16,18 @@ Net::SSLeay::X509_CRL class in L<SPVM> represents L<X509_CRL|https://docs.openss
 
   use Net::SSLeay::X509_CRL;
 
+=head1 Class Methods
+
+=head2 new
+
+C<static method new : L<Net::SSLeay::X509_CRL|SPVM::Net::SSLeay::X509_CRL> ();>
+
+Calls native L<X509_CRL_new|https://docs.openssl.org/1.1.1/man3/X509_dup/> function, creates a new  L<Net::SSLeay::X509_CRL|SPVM::Net::SSLeay::X509_CRL> object, sets the pointer value of the object to the return value of the native function, and returns the new object.
+
+Exceptions:
+
+If X509_CRL_new failed, an exception is thrown with C<eval_error_id> set to the basic type ID of L<Net::SSLeay::Error|SPVM::Net::SSLeay::Error> class.
+
 =head1 Instance Methods
 
 C<method DESTROY : void ();>
