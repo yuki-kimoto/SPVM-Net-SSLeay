2981a9de5883be5e0f3984b6e2b240b27c4b1f47
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-12-05 10:09:34 +0900

    Add Net::SSLeay::X509_VERIFY_PARAM#new method.

--- lib/SPVM/Net/SSLeay/X509_VERIFY_PARAM.c
+++ lib/SPVM/Net/SSLeay/X509_VERIFY_PARAM.c
@@ -8,6 +8,35 @@
 
 static const char* FILE_NAME = "Net/SSLeay/X509_VERIFY_PARAM.c";
 
+int32_t SPVM__Net__SSLeay__X509_VERIFY_PARAM__new(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  int32_t error_id = 0;
+  
+  X509_VERIFY_PARAM* self = X509_VERIFY_PARAM_new();
+  
+  if (!self) {
+    int64_t ssl_error = ERR_peek_last_error();
+    
+    char* ssl_error_string = env->get_stack_tmp_buffer(env, stack);
+    ERR_error_string_n(ssl_error, ssl_error_string, SPVM_NATIVE_C_STACK_TMP_BUFFER_SIZE);
+    
+    env->die(env, stack, "[OpenSSL Error]X509_VERIFY_PARAM_new failed:%s.", ssl_error_string, __func__, FILE_NAME, __LINE__);
+    
+    int32_t tmp_error_id = env->get_basic_type_id_by_name(env, stack, "Net::SSLeay::Error", &error_id, __func__, FILE_NAME, __LINE__);
+    if (error_id) { return error_id; }
+    error_id = tmp_error_id;
+    
+    return error_id;
+  }
+  
+  void* obj_self = env->new_pointer_object_by_name(env, stack, "Net::SSLeay::X509_VERIFY_PARAM", self, &error_id, __func__, FILE_NAME, __LINE__);
+  if (error_id) { return error_id; }
+  
+  stack[0].oval = obj_self;
+  
+  return 0;
+}
+
 int32_t SPVM__Net__SSLeay__X509_VERIFY_PARAM__get_flags(SPVM_ENV* env, SPVM_VALUE* stack) {
   
   int32_t error_id = 0;
--- lib/SPVM/Net/SSLeay/X509_VERIFY_PARAM.pm
+++ lib/SPVM/Net/SSLeay/X509_VERIFY_PARAM.pm
@@ -16,6 +16,18 @@ Net::SSLeay::X509_VERIFY_PARAM class in L<SPVM> represents L<X509_VERIFY_PARAM|h
 
   use Net::SSLeay::X509_VERIFY_PARAM;
 
+=head1 Class Methods
+
+=head2 new
+
+C<static method new : L<Net::SSLeay::X509_VERIFY_PARAM|SPVM::Net::SSLeay::X509_VERIFY_PARAM> ();>
+
+Calls native L<X509_VERIFY_PARAM_new|https://man.openbsd.org/X509_VERIFY_PARAM_inherit.3> function, creates a new  L<Net::SSLeay::X509_VERIFY_PARAM|SPVM::Net::SSLeay::X509_VERIFY_PARAM> object, sets the pointer value of the object to the return value of the native function, and returns the new object.
+
+Exceptions:
+
+If X509_VERIFY_PARAM_new failed, an exception is thrown with C<eval_error_id> set to the basic type ID of L<Net::SSLeay::Error|SPVM::Net::SSLeay::Error> class.
+
 =head1 Instance Methods
 
 =head2 get_flags
