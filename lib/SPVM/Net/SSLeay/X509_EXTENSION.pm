ee534d682a19b6db8d2b77eb5869f0e990fcad30
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-12-06 10:33:25 +0900

    Add Net::SSLeay::XX509_EXTENSION#new method.

--- lib/SPVM/Net/SSLeay/X509_EXTENSION.pm
+++ lib/SPVM/Net/SSLeay/X509_EXTENSION.pm
@@ -16,6 +16,18 @@ Net::SSLeay::X509_EXTENSION class in L<SPVM> represents L<X509_EXTENSION|https:/
 
   use Net::SSLeay::X509_EXTENSION;
 
+=head1 Class Methods
+
+=head2 new
+
+C<static method new : L<Net::SSLeay::X509_EXTENSION|SPVM::Net::SSLeay::X509_EXTENSION> ();>
+
+Calls native L<X509_EXTENSION_new|https://docs.openssl.org/3.1/man3/X509_dup/> function, creates a new  L<Net::SSLeay::X509_EXTENSION|SPVM::Net::SSLeay::X509_EXTENSION> object, sets the pointer value of the object to the return value of the native function, and returns the new object.
+
+Exceptions:
+
+If X509_EXTENSION_new failed, an exception is thrown with C<eval_error_id> set to the basic type ID of L<Net::SSLeay::Error|SPVM::Net::SSLeay::Error> class.
+
 =head1 Instance Methods
 
 =head2 get_data
