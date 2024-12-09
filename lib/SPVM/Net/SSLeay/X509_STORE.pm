4035c1aa590f14b368a8b6c41b89c24eafa36788
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-12-09 09:29:08 +0900

    Add Net::SSLeay::X509_STORE#new method.

--- lib/SPVM/Net/SSLeay/X509_STORE.pm
+++ lib/SPVM/Net/SSLeay/X509_STORE.pm
@@ -16,6 +16,18 @@ Net::SSLeay::X509_STORE in L<SPVM> represetns L<X509_STORE|https://docs.openssl.
 
   use Net::SSLeay::X509_STORE;
 
+=head1 Class Methods
+
+=head2 new
+
+C<static method new : L<Net::SSLeay::X509_STORE|SPVM::Net::SSLeay::X509_STORE> ();>
+
+Calls native L<X509_STORE_new|https://docs.openssl.org/1.1.1/man3/X509_STORE_new/> function, creates a new  L<Net::SSLeay::X509_STORE|SPVM::Net::SSLeay::X509_STORE> object, sets the pointer value of the object to the return value of the native function, and returns the new object.
+
+Exceptions:
+
+If X509_STORE_new failed, an exception is thrown with C<eval_error_id> set to the basic type ID of L<Net::SSLeay::Error|SPVM::Net::SSLeay::Error> class.
+
 =head1 Instance Methods
 
 =head2 set_flags
