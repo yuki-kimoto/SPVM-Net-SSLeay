c51ba32821d3d6137b6e0bdc1bdea62e3bfc955c
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-02-05 09:16:27 +0900

    Add SSL_ERROR_ZERO_RETURN handling to shutdown in both C and SPVM layers to prevent unnecessary exceptions and potential hangs during connection closure.

--- lib/SPVM/Net/SSLeay.c
+++ lib/SPVM/Net/SSLeay.c
@@ -755,6 +755,9 @@ int32_t SPVM__Net__SSLeay__shutdown(SPVM_ENV* env, SPVM_VALUE* stack) {
       if (error_id) { return error_id; }
       error_id = tmp_error_id;
     }
+    else if (ssl_operation_error == SSL_ERROR_ZERO_RETURN) {
+      error_id = env->get_basic_type_id_by_name(env, stack, "Net::SSLeay::Error::SSL_ERROR_ZERO_RETURN", &error_id, __func__, FILE_NAME, __LINE__);
+    }
     else {
       int32_t tmp_error_id = env->get_basic_type_id_by_name(env, stack, "Net::SSLeay::Error", &error_id, __func__, FILE_NAME, __LINE__);
       if (error_id) { return error_id; }
